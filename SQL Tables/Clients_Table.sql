399cd21544abe442b040e5aa240f20cbc6aaf3f7
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-03-07 00:03:07 +0200

    Add Clients Database's Table query.

---
+++ SQL Tables/Clients_Table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Clients
+(
+  id SERIAL UNIQUE NOT NULL,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  hashedpassword VARCHAR(255) NOT NULL ,
+  salt VARCHAR(255) NOT NULL ,
+  fullname VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
