97ba887b09ba48d66ad1e87d9f5149f7463bbc02
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-03-12 01:30:46 +0200

    Add Encryption.java

---
+++ src/Encryption.java
@@ -0,0 +1,25 @@
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Encryption {
+
+    public static String getHashMD5(String unhashed, String salt) {
+        // Hash the password.
+        final String toHash = salt + unhashed + salt;
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException ex) {
+            return "00000000000000000000000000000000";
+        }
+        messageDigest.update(toHash.getBytes(), 0, toHash.length());
+        String hashed = new BigInteger(1, messageDigest.digest()).toString(16);
+        if (hashed.length() < 32) {
+            hashed = "0" + hashed;
+        }
+        return hashed.toUpperCase();
+    }
+
+}
